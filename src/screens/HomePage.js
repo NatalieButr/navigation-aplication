d9b001dcd637532562cadbfa158c09bb8214cef7
Natalie Butrim <natashabutrim@gmail.com>
2020-01-24 13:03:28 +0300

    create folders screens and components

---
+++ src/screens/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+class HomePage extends  React.Component {
+
+    render() {
+        return (
+            <div>
+                <button className='enter-btn'></button>
+            </div>
+        )
+    }
+
+}
