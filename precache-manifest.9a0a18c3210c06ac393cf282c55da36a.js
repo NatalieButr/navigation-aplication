e00a8b15c8bde2aec0a4555c1ab199c9a65fbd1a
NatalieButr <natashabutrim@gmail.com>
2020-04-03 11:51:34 +0300

    Updates

---
+++ precache-manifest.9a0a18c3210c06ac393cf282c55da36a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a260142cfad1ee804d3c443d13d59c",
+    "url": "/navigation-aplication/index.html"
+  },
+  {
+    "revision": "69f331938e7ab41329c4",
+    "url": "/navigation-aplication/static/css/main.7e3a80c4.chunk.css"
+  },
+  {
+    "revision": "d9c3af91abd4612f8dbc",
+    "url": "/navigation-aplication/static/js/2.ca903d22.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/navigation-aplication/static/js/2.ca903d22.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69f331938e7ab41329c4",
+    "url": "/navigation-aplication/static/js/main.c5bea919.chunk.js"
+  },
+  {
+    "revision": "3edc5c48a87fd0cadf77",
+    "url": "/navigation-aplication/static/js/runtime-main.452e039e.js"
+  },
+  {
+    "revision": "2e573824d92ca52a919c4201a0f2c204",
+    "url": "/navigation-aplication/static/media/logo.2e573824.svg"
+  }
+]);
